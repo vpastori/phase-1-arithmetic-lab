03bdc5390749fd4d98cc62ef1a18bd27f4090346
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-03-19 14:27:13 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
+let num1 = 31;
+let num2 = 2; 
+let multiply = num1 * num2;
+console.log(multiply);
 // Write your code here
+let random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 16
+let num4 = 6
+let num5 = 20
+let num6 = 15
+let num7 = 12
+
+let mod = (num3 % num4);
+console.log(mod);
+
+let max = Math.max(num5, num6, num7);
+console.log(max);
